087649d55f6b24fe2c70ea226705c0651fc5928b
Shamim Reza <wpshamim@gmail.com>
2021-03-11 22:52:44 +0600

    update not found page

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NotFound.css'
 
 const NotFound = () => {
@@ -8,6 +9,7 @@ const NotFound = () => {
                 <h1>404</h1>
                 <h3>Not Found</h3>
                 <p>The resource requested could not be found on this server!</p>
+                <Link className="go-back" to="/">Back to Your Home 😄 </Link>
             </div>
         </div>
     );
